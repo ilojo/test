540d6580091db78dd6a8b800ed97ea5239cb82b4
ilojo <118895454+ilojo@users.noreply.github.com>
2022-12-01 12:19:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,34 +13,13 @@ struct tiedot{
     char sukunimi[20];
     int ika;
 };
-void lista()
-{
-
-    try{
-        ifstream lista1("testi2.txt");
-
-        if(!lista1){
-        cerr << "Ei aukea!! 'testi2.txt'!" << endl;
-        exit(1);
-}
-        string line;
-        list<string> listani;
-
-            while (getline(lista1,line)){
-            listani.push_back(line);
-            cout << listani.back() << endl;
-}
-}
-        catch(const exception& ex){}
-}
-
 void lisenssi()
 {
        tiedot s;
-       ofstream tiedosto("testi2.txt", ios::app);
+       ofstream tiedosto("C:/testi/testi2.txt", ios::app);
 
        if(tiedosto.is_open()){
-      cout << "Kirjoita etunimesi:  ";
+       cout << "Kirjoita etunimesi:  ";
             cin>>s.etunimi;
             tiedosto<<s.etunimi<< ' ';
        cout << "Kirjoita sukunimesi: ";
@@ -50,17 +29,78 @@ void lisenssi()
             cin>>s.ika;
             tiedosto<<s.ika<< ' ';
        cout << "Nimesi on " << s.etunimi << " " << s.sukunimi<<" " << s.ika<<'\n';
-
        cout <<"lisenssi numerosi on: ";
        cout << s.numero<<"\n";
             tiedosto<<s.numero<< endl;
+
+
+}
+       tiedosto.close();
+}
+
+void haku(){
+
+       char line[500];
+       char haku1[20];
+       int i;
+
+       ifstream tiedosto;
+
+       tiedosto.open("C:/testi/testi2.txt");
+       cout<<"Haku: ";
+       cin>>haku1;
+
+        if (tiedosto.is_open() ){
+        while ( !tiedosto.eof() ){
+            tiedosto.getline(line, 500, ',');
+            for (i = 0;line[i] == haku1[i];i++)
+{
+                if (haku1[i] == 0)
+{
+                    cout << "Osuma!!" << endl;
+                    cout << line << endl;
+}
+}
+            for (int j = 0;j < 3;j++) tiedosto.getline(line, 500, ',');
+            tiedosto.getline(line, 500, '\n');
+}
+                    cout << "Ei osumaa!!" << endl;
+}
+    else
+{
+                     cout << "Ei aukee!!" << endl;
+}
             tiedosto.close();
 }
+
+void lista()
+{
+       ifstream tiedosto("C:/testi/testi2.txt");
+
+       if(!tiedosto){
+
+       cout << "Ei aukea!!!" << endl;
+       exit(1);
+
 }
+       string line;
+       list<string> listani;
+
+           while (getline(tiedosto,line)){
+           listani.push_back(line);
+
+
+           cout << listani.back()  << endl;
+}
+           tiedosto.close();
+
+
+}
+
 void tyhjennys()
 {
         ifstream tiedosto;
-        string tiedosto2 = "testi2.txt";
+        string tiedosto2 = "C:/testi/testi2.txt";
 
         tiedosto.open(tiedosto2.c_str(), ifstream::out | ifstream::trunc );
         cout << "Tyhjennetty!"<< endl;
@@ -74,7 +114,7 @@ int main(){
 srand(time(NULL));
         int valinta;
         do{
-            cout << "0. Lopeta" <<endl <<"1. Hanki lisenssi\n" <<"2. Lista\n"<<"3. Listan tyhjennys\n";
+            cout << "0. Lopeta" <<endl <<"1. Hanki lisenssi\n" <<"2. Lista\n"<<"3. Listan tyhjennys\n" << "4. Haku\n";
             cin >> valinta;
             switch(valinta){
                 case 0:
@@ -82,13 +122,16 @@ srand(time(NULL));
                 return 0;
                 case 1:
                 lisenssi();
-                return 0;
+                return main();
                 case 2:
                 lista();
-                return 0;
+                return main();
                 case 3:
                 tyhjennys();
-                return 0;
+                return main();
+                case 4:
+                haku();
+                return main();
 }
 
 }
